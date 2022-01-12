4209aac3277523a7e363daaf8bd761465023d3da
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2022-01-12 18:53:26 -0500

    added handlebars functionality and homepage view

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+const path = require("path");
 const express = require("express");
-const routes = require("./routes");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// handlebars.js set up
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// use stylesheet
+app.use(express.static(path.join(__dirname, "public")));
+
+//
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
